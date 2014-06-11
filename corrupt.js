5093974ae327b1b00815edda7de1136b4e624ddd
Juan Batiz-Benet <juan@benet.ai>
2014-06-11 00:37:35 -0700

    changed construction api

--- corrupt.js
+++ corrupt.js
@@ -1,13 +1,15 @@
+var extend = require('xtend')
 var transDuplex = require('duplex-transform')
 var shuffle = require('shuffle-buffer')
 
 // returns a stream wrapping an integrity WireProtocol
 // that corrupts packets with a certain probability.
-module.exports = function CorruptWire(probability, integrityWire) {
+module.exports = function CorruptWire(integrityWire, opts) {
+  opts = extend({probability: 0.5}, opts || {})
   return transDuplex.obj(corruptChecksum, integrityWire, corruptPayload)
 
   function corruptChecksum(msg, enc, next) {
-    if (Math.random() < (probability / 2.0)) {
+    if (Math.random() < (opts.probability / 2.0)) {
       msg.checksum = msg.calculateChecksum()
       msg.checksum = shuffle(msg.checksum)
     }
@@ -16,7 +18,7 @@ module.exports = function CorruptWire(probability, integrityWire) {
   }
 
   function corruptPayload(msg, enc, next) {
-    if (Math.random() < (probability / 2.0))
+    if (Math.random() < (opts.probability / 2.0))
       msg.payload = shuffle(msg.payload)
     this.push(msg)
     next()
--- protocol.js
+++ protocol.js
@@ -1,3 +1,4 @@
+var extend = require('xtend')
 var transDuplex = require('duplex-transform')
 var msgproto = require('msgproto')
 var IntegrityFrame = require('./frame')
@@ -9,19 +10,27 @@ IntegrityProtocol.Corrupt = require('./corrupt')
 
 // integrity stream "wraps" (pipes) a duplex packet stream
 // with integrity checksums + verification
-function IntegrityProtocol(checksumFn, payloadType, stream) {
-  checksumFn = IntegrityFrame.coerceChecksumFn(checksumFn)
+function IntegrityProtocol(stream, opts) {
+  // default opts
+  opts = extend({unwrap: true, checksumFn: 'sha1'}, (opts || {}))
+
+  if (!opts.payloadType)
+    throw new Error('requires opts.payloadType')
+
+  // coerce might throw:
+  opts.checksumFn = IntegrityFrame.coerceChecksumFn(opts.checksumFn)
+
   return transDuplex.obj(wrap, stream, unwrap)
 
   function wrap(msg, enc, next) {
-    this.push(IntegrityFrame(checksumFn, msg, payloadType))
+    this.push(IntegrityFrame(opts.checksumFn, msg, opts.payloadType))
     next()
   }
 
   // acceptFns is a list of functions to accept as valid.
   // if acceptFns is undefined, all functions in multihash accepted
   function unwrap(msg, enc, next) {
-    msg.payloadType = msg.payloadType || payloadType
+    msg.payloadType = msg.payloadType || opts.payloadType
 
     var err = undefined
     try {
@@ -34,7 +43,11 @@ function IntegrityProtocol(checksumFn, payloadType, stream) {
       this.emit('invalid', { message: msg, error: err })
     } else {
       // ok, it's good. unwrap.
-      this.push(msg.getDecodedPayload())
+
+      if (opts.unwrap)
+        this.push(msg.getDecodedPayload())
+      else
+        this.push(msg)
     }
     next()
   }
